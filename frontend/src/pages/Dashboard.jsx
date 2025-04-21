835185e63b530481e1f973ca5db4d3c8345fb4ad
project3 <manhhungnguyen.zwh@gmail.com>
2025-04-21 07:13:04 +0700

    khung frontend va backend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,52 +1,81 @@
-import React from 'react'
-import { NavLink, Outlet, useNavigate } from 'react-router-dom'
-import { assets } from '../assets/assets'
+import React, { useContext } from "react";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
 
 const Dashboard = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
+  const { companyData } = useContext(AppContext);
   return (
     <div>
-        {/* Navbar for Recruiter Panel */}
-        <div className='shadow p-4'>
-            <div className='row '>
-                <div className='col-auto me-auto'>
-                    <img onClick={() => navigate('/')} src={assets.logo} style={{cursor: "pointer"}} />
-                </div>
-                <div className='col-auto d-flex gap-3 align-items-center'>
-                    <p className='mb-0'>Welcome,  Nguyen Hung</p>
-                    <div className='d-flex gap-2'>
-                        <img src={assets.company_icon} alt="" />
-                        <button className='btn border noflexwrap'   >Log out</button>
-                    </div>
-                </div>
+      {/* Navbar for Recruiter Panel */}
+      <div className="shadow p-4">
+        <div className="row ">
+          <div className="col-auto me-auto">
+            <img
+              onClick={() => navigate("/")}
+              src={assets.logo}
+              style={{ cursor: "pointer" }}
+            />
+          </div>
+          {companyData && (
+            <div className="col-auto d-flex gap-3 align-items-center">
+              <p className="mb-0">Welcome, {companyData.name}</p>
+              <div className="d-flex gap-2">
+                <img src={companyData.image} alt="" />
+                <button className="btn border noflexwrap">Log out</button>
+              </div>
             </div>
+          )}
         </div>
-        {/* Left side bar with option to add job, manage job, view applications */}
-        <div>
-            <div className='row mt-2'>
-                <div className='col-2 flex-column gap-3'>
-                    <ul>
-                        <NavLink to={'/dashboard/add-job'} className={({isActive}) => `d-flex gap-2 align-items-center p-3 link-underline link-underline-opacity-0 rounded ${isActive && 'bg-primary bg-opacity-25 '}`}>
-                            <img src={assets.add_icon} alt="" />
-                            <p className='mb-0 text-dark'>Add Job</p>
-                        </NavLink>
-                        <NavLink to={'/dashboard/manage-jobs'} className={({isActive}) => `d-flex gap-2 align-items-center p-3 link-underline link-underline-opacity-0 rounded ${isActive && 'bg-primary bg-opacity-25'}`}>
-                            <img src={assets.home_icon} alt="" />
-                            <p className='mb-0 text-dark'>Manage Jobs</p>
-                        </NavLink>
-                        <NavLink to={'/dashboard/view-applications'} className={({isActive}) => `d-flex gap-2 align-items-center p-3 link-underline link-underline-opacity-0 rounded ${isActive && 'bg-primary bg-opacity-25'}`}>
-                            <img src={assets.person_tick_icon} alt="" />
-                            <p className='mb-0 text-dark'>View applications</p>
-                        </NavLink>
-                    </ul>
-                </div>
-                <div className='col-10'>
-                    <Outlet />
-                </div>
-            </div>
+      </div>
+      {/* Left side bar with option to add job, manage job, view applications */}
+      <div>
+        <div className="row mt-2">
+          <div className="col-2 flex-column gap-3">
+            <ul>
+              <NavLink
+                to={"/dashboard/add-job"}
+                className={({ isActive }) =>
+                  `d-flex gap-2 align-items-center p-3 link-underline link-underline-opacity-0 rounded ${
+                    isActive && "bg-primary bg-opacity-25 "
+                  }`
+                }
+              >
+                <img src={assets.add_icon} alt="" />
+                <p className="mb-0 text-dark">Add Job</p>
+              </NavLink>
+              <NavLink
+                to={"/dashboard/manage-jobs"}
+                className={({ isActive }) =>
+                  `d-flex gap-2 align-items-center p-3 link-underline link-underline-opacity-0 rounded ${
+                    isActive && "bg-primary bg-opacity-25"
+                  }`
+                }
+              >
+                <img src={assets.home_icon} alt="" />
+                <p className="mb-0 text-dark">Manage Jobs</p>
+              </NavLink>
+              <NavLink
+                to={"/dashboard/view-applications"}
+                className={({ isActive }) =>
+                  `d-flex gap-2 align-items-center p-3 link-underline link-underline-opacity-0 rounded ${
+                    isActive && "bg-primary bg-opacity-25"
+                  }`
+                }
+              >
+                <img src={assets.person_tick_icon} alt="" />
+                <p className="mb-0 text-dark">View applications</p>
+              </NavLink>
+            </ul>
+          </div>
+          <div className="col-10">
+            <Outlet />
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
